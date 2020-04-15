af8f325410b93ba3bd0fc56010965ca6504fd767
Irelia <yaokai729@outlook.com>
2020-04-15 19:45:08 +0800

    deploy

---
+++ precache-manifest.a1fc9edc8da12cef265a0d6c9646fa52.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0963fafae9794e52bd8d",
+    "url": "/money-xixi/css/app.735f326c.css"
+  },
+  {
+    "revision": "db358e4a4cbdbb6a75fb",
+    "url": "/money-xixi/css/chunk-vendors.b6820902.css"
+  },
+  {
+    "revision": "9cfd9dd874c80cabcacdbafee685783e",
+    "url": "/money-xixi/img/avatar.9cfd9dd8.jpg"
+  },
+  {
+    "revision": "423e4647994afbf11d378ff3808b4f57",
+    "url": "/money-xixi/img/chart.423e4647.jpg"
+  },
+  {
+    "revision": "cc04b3cfdb9735fb75e50eb399d350cf",
+    "url": "/money-xixi/img/edit.cc04b3cf.jpg"
+  },
+  {
+    "revision": "12c2fee172f5febd9ff99ba2f3c28500",
+    "url": "/money-xixi/img/home.12c2fee1.jpg"
+  },
+  {
+    "revision": "fbea89a956facbfbe5838e6ab6be9ca0",
+    "url": "/money-xixi/img/money-box.fbea89a9.png"
+  },
+  {
+    "revision": "9a9eb2f77d5db29ef1b5f7b259d7b339",
+    "url": "/money-xixi/img/qrcode.9a9eb2f7.png"
+  },
+  {
+    "revision": "75aa127ce731b906aca883d20397c00f",
+    "url": "/money-xixi/index.html"
+  },
+  {
+    "revision": "0963fafae9794e52bd8d",
+    "url": "/money-xixi/js/app.159ec089.js"
+  },
+  {
+    "revision": "db358e4a4cbdbb6a75fb",
+    "url": "/money-xixi/js/chunk-vendors.476e7133.js"
+  },
+  {
+    "revision": "6311977b50e94b7f07d8aad9bb273e0b",
+    "url": "/money-xixi/manifest.json"
+  },
+  {
+    "revision": "264fdea9962ec8f9f796227016317eff",
+    "url": "/money-xixi/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-xixi/robots.txt"
+  }
+]);
